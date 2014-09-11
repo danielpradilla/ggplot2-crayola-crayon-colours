87585eb3c9864937f19109e9a94fc116f2805738
Daniel Pradilla <info@danielpradilla.info>
2014-09-11 13:29:53 +0200

    readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Shiny app based on [http://learnr.wordpress.com/2010/01/21/ggplot2-crayola-crayon-colours/](http://learnr.wordpress.com/2010/01/21/ggplot2-crayola-crayon-colours/).
 
 Gets list of [Crayola Colors from Wikipedia](http://en.wikipedia.org/wiki/List_of_Crayola_crayon_colors) and displays it in a chart.
+
+Working (?) demo at [https://danielpradilla.shinyapps.io/ggplot2-crayola-crayon-colours/](https://danielpradilla.shinyapps.io/ggplot2-crayola-crayon-colours/)
--- ggplot-crayola.R
+++ ggplot-crayola.R
@@ -4,12 +4,12 @@ library(plyr)
 library(colorspace)
 
 theurl <- "http://en.wikipedia.org/wiki/List_of_Crayola_crayon_colors"
-html <- htmlParse(theurl)
+html <- htmlParse(theurl, encoding="UTF-8")
 crayola <- readHTMLTable(html, stringsAsFactors = FALSE)[[1]]
 crayola <-crayola[,c("Hex Code\n(approximate)†", "Issued", "Retired")]
 names(crayola) <- c("colour", "issued", "retired")
 crayola <- crayola[!duplicated(crayola$colour),]
-crayola$retired[crayola$retired == ""] <- 2014
+crayola$retired[crayola$retired == ""] <- as.numeric(format(Sys.Date(),"%Y"))
 
 #Instead of geom_rect() I will show two options of plotting the same data using geom_bar() and geom_area() 
 #to plot the data, and need to ensure that there’s one entry per colour per year it was(is) in the production.
@@ -18,6 +18,10 @@ colours <- ddply(crayola, .(colour), transform, year = issued:retired)
 
 
 p1 <- ggplot(colours, aes(year, 1, fill = colour)) + geom_area(position = "fill", colour = "white") + theme_bw() + scale_fill_identity()
+
+
+labels <- c(1903, 1949, 1958, 1972, 1990, 1998, as.numeric(format(Sys.Date(),"%Y")))
+breaks <- labels - 1
 
 x <- scale_x_continuous("", breaks = breaks, labels = labels, expand = c(0, 0))
 y <- scale_y_continuous("", expand = c(0, 0))
